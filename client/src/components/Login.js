d1eaf6a3550bb608fe66ec0fb7fdd13b721ce559
Gurjot-0031 <gurjot5076@gmail.com>
2020-05-07 03:39:14 -0400

    Login - Fetch issue fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -15,18 +15,21 @@ export default class Login extends Component{
     }
 
 
-     login = () => {
+     login() {
         console.log("Testing.......");
+
+
          fetch("http://localhost:8080/rest/users/authenticate",{
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'no-cors', // no-cors, *cors, same-origin
+            //mode: 'no-cors', // no-cors, *cors, same-origin
             //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             //credentials: 'include', // include, *same-origin, omit
             headers: {
                 'Content-Type': 'application/json',
-                'Accept':  'application/json',
+                'Accept':  '*/*',
+                //'Cache-Control': 'no-cache'
                 //"Vary":"Access-Control-Request-Headers"
-                'Origin':'*'
+                //'Origin':'*'
                 //'Content-Type': 'application/x-www-form-urlencoded',
             },
             //redirect: 'follow', // manual, *follow, error
@@ -35,10 +38,11 @@ export default class Login extends Component{
         })
             .then(response=> response.json())
             .then(parsedData => {
-                console.log(parsedData); // JSON data parsed by `response.json()` call
+                console.log('parsed data ' +parsedData.username+" "+parsedData.isSuccess); // JSON data parsed by `response.json()` call
                 return parsedData;
             })
-             .then(data=>this.setState({loggedUsername:data.username,isLoggedIn:true}))
+             //.then(data=>this.setState({loggedUsername:data.username,isLoggedIn:true}))
+             .then(data=>localStorage.setItem("data",data))
             .catch(error=>console.log("CAUGHT ERROR "+error));
     }
     render() {
@@ -57,22 +61,23 @@ export default class Login extends Component{
                 {/*    auth ? <Redirect to="home"></Redirect>:null*/}
                 {/*}*/}
                 <div>
-                    <form onSubmit={this.login} >
+                    <div className="materialboxed">
                         <label>
                             <input type={"text"} name={"username"}
                                    value={this.state.username}
-                                   onChange={(e)=>
-                                   {this.setState({username:e.target.value})}} required>
+                                   onChange={this.handleUsernameChange.bind(this)} required>
                             </input>
                         </label>
                         <label>
                             <input type={"text"} name={"password"}
                                    value={this.state.password}
-                                   onChange={(e)=>
-                                   {this.setState({password:e.target.value})}} required>
+                                   onChange={this.handlePasswordChange.bind(this)} required>
                             </input>
                         </label>
-                        <input type={"submit"} value={"LOGIN"}/>
+                        <input type="button"
+                               onClick={this.login.bind(this)}
+                               className="fixed-action-btn" value={"LOGIN"}
+                        />
                         {/*<center>*/}
                         {/*    <button type={"submit"} >LOGIN</button>*/}
                         {/*    /!*<p> New User?  <BrowserRouter><Link compo>Sign Up</Link></BrowserRouter> </p>*!/*/}
@@ -81,10 +86,17 @@ export default class Login extends Component{
                         {/*    <button onClick={()=>this.setState({isRegistering:true})}>Go to REGISTER</button>*/}
                         {/*    /!*<p> New User?  <BrowserRouter><Link compo>Sign Up</Link></BrowserRouter> </p>*!/*/}
                         {/*</center>*/}
-                    </form>
+                    </div>
                 </div>
             </div>
             </div>
         );
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username:event.target.value});
+    }
+    handlePasswordChange(event) {
+        this.setState({password:event.target.value});
     }
 }
