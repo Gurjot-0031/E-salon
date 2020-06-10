5fddfa49b3ed0a374ddf33554a860b58d16a1d1e
Gurjot-0031 <gurjot5076@gmail.com>
2020-06-10 12:22:05 -0400

    Refactoring in BookAppointment.js

--- client/src/components/BookAppointment.js
+++ client/src/components/BookAppointment.js
@@ -38,67 +38,69 @@ export  default class BookAppointment extends Component {
                <div className="container">
                     <h5 className="left-align">Your selected services..</h5>
                    {(this.state.items.length !== 0)
-                       ?<div className={"row"}>
-                           <div className={"col s3"}>Service</div>
-                           <div className={"col s3"}>Price</div>
-                           <div className={"col s3"}>Estimated time</div>
-                           <div className={"col s3"}>Remove</div>
+                       ?<div>
+                           <div className={"row"}>
+                            <div className={"col s3"}>Service</div>
+                            <div className={"col s3"}>Price</div>
+                            <div className={"col s3"}>Estimated time</div>
+                            <div className={"col s3"}>Remove</div>
+                           </div>
+
+                           {this.state.items.map(item=>(
+                           <div className={"row"} key={item.id}>
+                               <div className={"col s3"}>
+                                   {item.name}
+                               </div>
+                               <div className={"col s3"}>
+                                   {item.price}
+                               </div>
+                               <div className={"col s3"}>
+                                   {item.estimatedTime}
+                               </div>
+                               <div className={"col s3"}>
+                                   {/*<i*/}
+                                   {/*    onClick={this.removeItem(item.id)}*/}
+                                   {/*   className="material-icons">delete</i>*/}
+                                   <button
+                                       className="btn-small"
+                                       onClick={()=>this.removeItem(item)}
+                                       //onClick={this.removeItem({item})}
+                                   >
+                                       REMOVE
+                                   </button>
+
+                               </div>
+                           </div>
+                           ))}
+                           <div>
+                               <div className={"row"}>
+                                   <div className={"col s6"}>Total price</div>
+                                   <div className={"col s2"}>${this.state.totalCost}</div>
+                               </div>
+                               <div className={"row"}>
+                                   <div className={"col s6"}>Total time</div>
+                                   <div className={"col s2"}>{this.state.totalTime} minutes</div>
+                               </div>
+                               <div className={"row schedulePicker"}>
+                                   <button
+                                       className={'btn'}
+                                       onClick={() => this.goToSchedulePicker()}>
+                                       NEXT
+                                   </button>
+                                   {/*<div className={"col s12"}>*/}
+                                   {/*    /!*{this.state.alreadyBooked.map(i=><p>{i.startDateTime}</p>)}*!/*/}
+                                   {/*    <SchedulePicker*/}
+                                   {/*        passedState = {this.state}*/}
+                                   {/*    />*/}
+                                   {/*</div>*/}
+                               </div>
+
+                           </div>
                        </div>
                        :<div>Your booking cart is empty</div>
                    }
-                    {this.state.items.map(item=>(
-                        <div className={"row"} key={item.id}>
-                            <div className={"col s3"}>
-                                {item.name}
-                            </div>
-                            <div className={"col s3"}>
-                                {item.price}
-                            </div>
-                            <div className={"col s3"}>
-                                {item.estimatedTime}
-                            </div>
-                            <div className={"col s3"}>
-                                {/*<i*/}
-                                {/*    onClick={this.removeItem(item.id)}*/}
-                                {/*   className="material-icons">delete</i>*/}
-                                <button
-                                    className="btn-small"
-                                    onClick={()=>this.removeItem(item)}
-                                    //onClick={this.removeItem({item})}
-                                    >
-                                    REMOVE
-                                </button>
-
-                            </div>
-                        </div>
-                    ))}
 
                 </div>
-                {(this.state.items.length !== 0)
-                    ?<div>
-                        <div className={"row"}>
-                            <div className={"col s6"}>Total price</div>
-                            <div className={"col s2"}>${this.state.totalCost}</div>
-                        </div>
-                        <div className={"row"}>
-                            <div className={"col s6"}>Total time</div>
-                            <div className={"col s2"}>{this.state.totalTime} minutes</div>
-                        </div>
-                        <div className={"row schedulePicker"}>
-                            <button onClick={() => this.goToSchedulePicker()}>
-                                NEXT
-                            </button>
-                            {/*<div className={"col s12"}>*/}
-                            {/*    /!*{this.state.alreadyBooked.map(i=><p>{i.startDateTime}</p>)}*!/*/}
-                            {/*    <SchedulePicker*/}
-                            {/*        passedState = {this.state}*/}
-                            {/*    />*/}
-                            {/*</div>*/}
-                        </div>
-
-                    </div>
-                    :null
-                }
                 {
                     (this.state.items.length!=0 && this.state.redirectToSchedulePicker)
                     ?
