c46afe1a0a38d61d9d9a360a6a7f1b049e4ab182
Gurjot-0031 <gurjot5076@gmail.com>
2020-06-17 22:24:18 -0400

    MyAccount - WIP

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import AppHeader from "./AppHeader";
+import  "../App.css";
 
 class MyAccount extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            editname :false,
+            editemail :false,
+            editphone :false,
         };
+        //this.edit = this.edit.bind(this);
+
     }
     componentDidMount(){
         fetch("http://localhost:8080/rest/users/getUserDetails", {
@@ -32,22 +38,55 @@ class MyAccount extends Component {
         return (
             <div>
                 <AppHeader/>
-                <table className='col s12'>
-                    <thead><div align='center'><th>My Account</th></div></thead>
+                <h4><div align='center'>My Account</div></h4>
+                <table className='MyAccountTable '>
+                    <tbody>
                     {
-                        Object.keys(this.state).map(i =>
-                            <tr key={i}>
-                                <td>{[i]}</td>
-                                <td>{this.state[i]}</td>
-                                <td><a ><i className='material-icons'>edit</i></a></td>
-                            </tr>
-                        )
+                        Object.keys(this.state)
+                            .filter(i => !i.startsWith('edit'))
+                            .map(i =>
+                                <tr key={i}>
+                                    <td className='col s2 '>{[i]}</td>
+                                    <td className='col s3' >{this.state[i]}</td>
+                                    {this.state['edit'+[i]] ? <td className='col s3'>
+                                    <textarea rows="1" cols="10"
+                                              style={{'resize':'none'}}
+                                              placeholder={'edit'+[i]+" "+this.state['edit'+[i]]}/>
+                                        </td>
+                                        :null}
+
+                                    {[i] == 'uid' || [i] == 'username' ? null : <td className='col s1'>
+                                        <button className='material-icons'
+                                                         onClick={()=>this.edit(i)}>
+                                            edit
+                                        </button>
+                                    </td>}
+                                </tr>
+                            )
                     }
+                    </tbody>
                 </table>
 
             </div>
         );
     }
+
+    edit(element) {
+        const x = "edit" + [element];
+        console.log("henlo2: "+x)
+        this.setState(prevState=>({
+            editname: !prevState.editname,
+        }))
+        console.log("HENLO "+this.state.editname);
+        //document.getElementById('text-'+[element]).hidden = false;
+        //alert([element]+"  "+this.state[element])
+    }
+
+
+
+
+
+
 }
 
 export default MyAccount;
